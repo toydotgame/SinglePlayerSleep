e2c3cb82cbf8ad795c521bd7891f87ec814f0aae
toydotgame <toydotgame@gmail.com>
2021-04-11 17:09:01 +1000

    Put main bed event class into another thread in order to allow server to continue whilst the Thread.sleep() command is run

--- src/io/github/toydotgame/SinglePlayerSleep/SPS.java
+++ src/io/github/toydotgame/SinglePlayerSleep/SPS.java
@@ -11,13 +11,18 @@ import org.bukkit.event.player.PlayerBedEnterEvent;
 public class SPS implements Listener {
 	@EventHandler
 	public void onPlayerSleep(PlayerBedEnterEvent bedEnterEvent){
-		Player player = bedEnterEvent.getPlayer();
-		String playerNameString = player.getName();
-		try {
-			Thread.sleep(5050);
-		} catch (InterruptedException e) {} // try/catch loops pain me...
-		World world = Bukkit.getWorld(player.getLocation().getWorld().getName());
-		world.setTime(0);
-		Bukkit.broadcastMessage(playerNameString + ChatColor.YELLOW + " slept.");
+		Thread sleepThread = new Thread() {
+			public void run() {
+				Player player = bedEnterEvent.getPlayer();
+				String playerNameString = player.getName();
+				try {
+					Thread.sleep(5050);
+				} catch (InterruptedException e) {} // try/catch loops pain me...
+				World world = Bukkit.getWorld(player.getLocation().getWorld().getName());
+				world.setTime(0);
+				Bukkit.broadcastMessage(playerNameString + ChatColor.YELLOW + " slept.");
+			}
+		};
+		sleepThread.start();
 	}
 }
